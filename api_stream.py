75ddf004f33c301d53e2024cb16c95e849384371
Jihyeon Park <jihyeona5525@gmail.com>
2024-07-23 16:51:24 +0900

    fix: txt 파일 전처리, Kkma->Kiwi, kiwi 명사 추출 후 추출 명사 없을 때 처리

--- api_stream.py
+++ api_stream.py
@@ -1,13 +1,11 @@
-from unstruct_step1 import Vec
+from unstruct_step1 import Vec, documents
 import asyncio
 from typing import AsyncIterable
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from langchain_community.document_loaders import TextLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
-from langchain_teddynote.retrievers import KkmaBM25Retriever
+from langchain_teddynote.retrievers import KiwiBM25Retriever
 from langchain.retrievers import EnsembleRetriever
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, StreamingResponse
@@ -19,7 +17,7 @@ from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from pydantic import BaseModel
-from konlpy.tag import Kkma
+from kiwipiepy import Kiwi
 from datetime import datetime
 import os
 
@@ -55,14 +53,18 @@ vector_db = Chroma(
     embedding_function = openai_embedding,
 )
 
-loader = TextLoader('대구공고전문.txt', encoding='utf-8')
-content = loader.load()[0].page_content
-text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200, separators = '\n')
-splitted_docs = text_splitter.split_text(content)
+kiwi = Kiwi()
+def noun_extractor(text):
+    results = []
+    result = kiwi.analyze(text)
+    for token, pos, _, _ in result[0][0]:
+        if len(token) != 1 and pos=='NNG' or pos=='NNP' or pos=='NR' or pos=="XR" or pos=='SL' or pos=='SH' or pos=='SN':
+            results.append(token)
+    return results
 
-kbm25_retriever = KkmaBM25Retriever.from_texts(splitted_docs, k=4)
-embed_retriever = vector_db.as_retriever(search_type="similarity", search_kwargs={"k": 4})
-ensemble_retriever = EnsembleRetriever(retrievers=[kbm25_retriever, embed_retriever], weights=[0.4, 0.6])
+kbm25_retriever = KiwiBM25Retriever.from_documents(documents, k=4)
+embed_retriever = vector_db.as_retriever(search_type="similarity", search_kwargs={"k": 2})
+ensemble_retriever = EnsembleRetriever(retrievers=[kbm25_retriever, embed_retriever], weights=[0.6, 0.4])
 
 class Message(BaseModel):
     content: str
@@ -75,12 +77,18 @@ async def send_message(content: str) -> AsyncIterable[str]:
     try:
         callback = AsyncIteratorCallbackHandler()
         
-        kkma = Kkma()
-        tok_query = kkma.nouns(content)
-        
-        docs = ensemble_retriever.invoke(' '.join(tok_query))
-        new_docs = [f"<Doc{i+1}>. {doc.page_content}" for i, doc in enumerate(docs)]
-        #print(new_docs)
+        tok_query = noun_extractor(content)
+        if len(tok_query) == 0:
+            tok_query = chat_history[-1]['tok_question']
+            question = content + ' ' + ' '.join(tok_query)
+        else:
+            question = ' '.join(tok_query)
+        print(question)
+
+        docs = ensemble_retriever.invoke(question)
+        new_docs = list(set(doc.page_content.replace('\t', ' ') for doc in docs))
+        filtered_docs = [f"<Doc{i+1}>. {d}" for i, d in enumerate(new_docs) if any(word in d for word in tok_query)]
+        print(filtered_docs)
         
         if not new_docs:
           raise NoDocumentsRetrievedError("No documents retrieved.")
@@ -93,13 +101,13 @@ async def send_message(content: str) -> AsyncIterable[str]:
         )
 
         year = datetime.now().year
-        history = "\n".join(f"Q: {item['question']}\nA: {item['answer']}" for item in chat_history[-2:])
+        history = "\n".join(f"Q: {item['question']}\nA(참고): {item['answer']}" for item in chat_history[-2:])
         #print(history)
         template = '''
         너는 대구공업고등학교 80년사 책의 내용과 관련된 질문에 답변하는 챗봇이야. 답변은 한국어 높임말로 써. 일관되고 친절한 말투를 써.
-        데이터에는 질문과 관련없는 내용도 있을 수 있기 때문에 확실한 내용만 답변해줘. 특정 사람에 대한 질문이면 확실히 사람을 구별해서 답변해줘. 올해는 {year}년이야.
-        DON'T MAKE UP THE ANSWER.
-        Data: {context}
+        데이터에는 질문과 관련없는 내용도 포함되어 있기 때문에 질문에 대한 확실한 답만을 찾아 답변해줘. 특정 사람과 관련된 질문이면 성과 이름이 "완벽히" 일치하는 자료를 찾아. 같은 이름을 가진 사람이 있어도 동명이인일 수 있기 떄문에 디테일을 보고 구별해서 알려줘(회수, 기수, 분야, 직업). 올해는 {year}년이야.
+        DON'T MAKE UP THE ANSWER. Don't repeat the answer from chat history.
+        Data(책 내용의 일부, don't connect docs with different numbers.): {context}
         Chat history: {history}
         Question: {question}
         Answer:
@@ -118,14 +126,14 @@ async def send_message(content: str) -> AsyncIterable[str]:
         chain = prompt | model | StrOutputParser()
         
         task = asyncio.create_task(
-            chain.ainvoke({"year": year, "context": new_docs, "question": content, "history": history})
+            chain.ainvoke({"year": year, "context": filtered_docs, "question": content, "history": history})
         )
 
         async for token in callback.aiter():
             yield token
 
         response = await task
-        chat_history.append({"question": content, "answer": response})
+        chat_history.append({"question": content, "tok_question": tok_query, "answer": response})
         if len(chat_history) > MAX_CHAT_HISTORY:
             chat_history.pop(0)
             
